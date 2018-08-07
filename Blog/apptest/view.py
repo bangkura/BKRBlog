a18d09897dcabb026774f88fafd24f72f75c6c19
Bangkura <tengyangjia@gmail.com>
2018-08-08 01:37:25 +0800

    Refactor the structure of the project

--- Blog/apptest/view.py
+++ Blog/apptest/view.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def hello(request):
     context = {}
     context['hello'] = 'hello world!'
-    return render(request, 'appTest/hello.html', context)
+    return render(request, 'apptest/hello.html', context)
--- Blog/settings.py
+++ Blog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + "/templates"],
+        'DIRS': [BASE_DIR + "/Blog/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Blog/static'),
 )
-
-
-#STATIC_ROOT = "/home/mysite/collect_static/"
 
 STATIC_URL = '/static/'
